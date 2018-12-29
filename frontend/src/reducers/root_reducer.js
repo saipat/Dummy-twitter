5bdac8dbb713b72599d5005eb495de0606e6ddf5
Cjewett_work <Craig9907@gmail.com>
2018-12-28 17:32:07 -0800

    Update font color to red

---
+++ frontend/src/reducers/root_reducer.js
@@ -0,0 +1,14 @@
+import {
+    combineReducers
+} from 'redux';
+import session from './session_reducer';
+import errors from './errors_reducer';
+import tweets from './tweets_reducer';
+
+const RootReducer = combineReducers({
+    session,
+    tweets,
+    errors
+});
+
+export default RootReducer;
